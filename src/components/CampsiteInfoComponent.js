5ec102f2fdca8f788d5bff5c3bd8a8bd45b28e8f
Blits <brianlapeyrouse98@gmail.com>
2021-10-29 22:22:50 -0500

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -26,7 +26,7 @@ function RenderCampsite({ campsite }) {
     )
 };
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({comments, postComment, campsiteId}) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -39,7 +39,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
                         </div>
                     );
                 })}
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         );
     }
@@ -68,8 +68,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        this.toggleModel();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.toggleModal();
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -190,7 +190,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments
+                        comments={props.comments}
+                        postComment={props.postComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
